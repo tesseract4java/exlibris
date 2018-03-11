e7542a0414dff9b36968869b345347311f289c44
Paul Vorbach <paul@vorba.ch>
2018-03-11 14:41:48 +0100

    Initialize project with Maven modules

---
+++ exlibris-gui/src/main/java/de/vorb/exlibris/gui/ExlibrisApplication.java
@@ -0,0 +1,13 @@
+package de.vorb.exlibris.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExlibrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExlibrisApplication.class, args);
+    }
+
+}
